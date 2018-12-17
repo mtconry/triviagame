0b49114e443a5911889403b07f8e447f841fbd3d
Mike Conry <mikeconry@Mikes-MacBook-Pro.local>
2018-12-17 18:38:40 -0500

    all files

---
+++ assets/java/app.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+var options = [
+    {
+        question: "Which NFL team has had the most SuperBowl wins?",
+        choice: ["Patriots", "Steelers", "Cowboys", "49ers"],
+        answer: 1,
+        photo: "assets/images/steelerrings.jpg", //https://i.pinimg.com/originals/3d/17/1a/3d171aad40ffd490f08066c21ff23b73.jpg
+    },
+    {
+        question: "Which NFL team has been to the most SuperBowls?",
+        choice: ["Patriots", "Steelers", "Cowboys", "49ers"],
+        answer: 0,
+        photo: "assets/images/patriotsapp.jpg", //http://www.xnojoe.com/wp-content/uploads/2017/02/img_9393.jpg
+    },
+    {
+        question: "Which NFL team has beaten the Patriots twice in the SuperBowl?",
+        choice: ["Patriots", "Steelers", "New York Giants", "Atlanta Falcons"],
+        answer: 2,
+        photo: "assets/images/incrediblecatch.jpg", //http://www.chicagonow.com/ex-posts-facto/files/2012/01/David-Tyree-e1327567419572.jpg
+    },
+    {
+        question: "What was the best comeback in SuperBowl history?",
+        choice: ["Patriots-Falcons", "Patriots-Seahawks", "Saints-Colts", "Redskins-Broncos"],
+        answer: 0,
+        photo: "assets/images/comeback-1.jpg", //https://boyslifeorg.files.wordpress.com/2017/09/comeback-1.jpg?w=620&h=465
+    },
+    {
+        question: "What was the best comeback in SuperBowl history?",
+        choice: ["Redskins-Broncos", "49ers-Broncos", "Bears-Patriots", "Cowboys-Bills"],
+        answer: 1,
+        photo: "assets/images/blowout.png", //https://www.gunaxin.com/wp-content/uploads/2013/01/Joe-Montana-Jerry-Rice-and-the-San-Francisco-49ers-Destroyed-John-Elway-and-the-Denver-Broncos-in-Super-Bowl-XXIV-560x315.png
+    },
+    {
+        question: "Who won the SuperBowl in 2005?",
+        choice: ["Colts", "Cowboys", "Ravens", "Saints"],
+        answer: 3,
+        photo: "assets/images/saints2005.jpg", // https://static01.nyt.com/images/2010/02/08/sports/08superbowl02/08superbowl02-articleLarge.jpg 
+    },
+    {
+        question: "Which of the Manning brothers have more SuperBowl Rings?",
+        choice: ["Peyton", "Eli", "The have the same amount"],
+        answer: 2,
+        photo: "assets/images/peyton-eli.jpg", // https://cdn.cnn.com/cnnnext/dam/assets/130913130555-peyton-eli-manning-story-top.jpg
+    },
+    {
+        question: "Which team has the most SuperBowl losses",
+        choice: ["Colts", "Cowboys", "Bills", "Broncos"],
+        answer: 3,
+        photo: "assets/images/losses.jpg", //https://i.imgflip.com/y26to.jpg
+    },
+    {
+        question: "Which team has the most SuperBowl losses",
+        choice: ["Colts", "Cowboys", "Bills", "Broncos"],
+        answer: 3,
+        photo: "assets/images/losses.jpg"
+    }
+
+
+
+
+] 
+})
